2d6805a02627b96793150f1f4f0a97d5af159dae
Pawaneet56 <pawaneetsingh56@gmail.com>
2020-12-24 21:17:43 +0530

    added confirm password

--- app/src/main/java/com/example/portfolioapp/Signupactivity.java
+++ app/src/main/java/com/example/portfolioapp/Signupactivity.java
@@ -20,7 +20,10 @@ public class Signupactivity extends AppCompatActivity {
     private EditText emailid;
     private EditText password;
     private Button signup;
+    private EditText cpassword;
     private FirebaseAuth auth;
+    private EditText name;
+
 
 
     @Override
@@ -31,33 +34,40 @@ public class Signupactivity extends AppCompatActivity {
         password=findViewById(R.id.password);
         signup=findViewById(R.id.signup);
         auth=FirebaseAuth.getInstance();
+        cpassword=findViewById(R.id.cpassword);
+        name=findViewById(R.id.name);
         signup.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 String txt_emailid=emailid.getText().toString();
                 String txt_password=password.getText().toString();
+                String Name=name.getText().toString();
+                String txt_cpassword=cpassword.getText().toString();
                 if(TextUtils.isEmpty(txt_emailid) || TextUtils.isEmpty(txt_password)){
                     Toast.makeText(Signupactivity.this,"Empty credentials",Toast.LENGTH_SHORT).show();
                 }
                 else if(txt_password.length()<6){
                     Toast.makeText(Signupactivity.this,"password too short",Toast.LENGTH_SHORT).show();
                 }
+                else if(!txt_password.equals(txt_cpassword)){
+                    Toast.makeText(Signupactivity.this,"Passwords do not match",Toast.LENGTH_SHORT).show();
+                }
                 else{
-                    registeruser(txt_emailid,txt_password);
+                    registeruser(txt_emailid,txt_password,Name);
                 }
 
             }
         });
     }
-    private void registeruser(String emailid,String password){
+    private void registeruser(String emailid,String password,String Name){
 auth.createUserWithEmailAndPassword(emailid,password).addOnCompleteListener(new OnCompleteListener<AuthResult>() {
     @Override
     public void onComplete(@NonNull Task<AuthResult> task) {
         if(task.isSuccessful()){
-            Toast.makeText(Signupactivity.this,"User succesfully registered",Toast.LENGTH_SHORT).show();
+            Toast.makeText(Signupactivity.this,"Thank you "+Name+", you are  succesfully registered",Toast.LENGTH_SHORT).show();
         }
         else{
-            Toast.makeText(Signupactivity.this,"sorry try again",Toast.LENGTH_SHORT).show();
+            Toast.makeText(Signupactivity.this,"sorry "+Name+", try again",Toast.LENGTH_SHORT).show();
         }
     }
 });
